e2ee2d7fbbec686ea5a28d12fd40fd26d56ecdf1
Marc Alo <APCmgalo48@gmail.com>
2015-03-25 21:16:43 +0800

    CRUDed Product  -modified Product/index.php, Product/view.php and ProductController.php   to fit frontend specifications.  -removed update and delete functions.

--- trunk/Application/marktroibags3.0/frontend/views/Product/index.php
+++ trunk/Application/marktroibags3.0/frontend/views/Product/index.php
@@ -15,22 +15,20 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <p>
-        <?= Html::a('Create Product', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+          //  ['class' => 'yii\grid\SerialColumn'],
 
             'user_id',
             'id',
             'name',
             'description',
 
-            ['class' => 'yii\grid\ActionColumn'],
+            ['class' => 'yii\grid\ActionColumn2'],
+            
         ],
     ]); ?>
 
