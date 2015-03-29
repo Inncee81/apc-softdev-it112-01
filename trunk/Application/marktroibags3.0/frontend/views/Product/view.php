eb0fc8b1da007ede5d55aeaa0cf6128b3be5e6cb
Marc Alo <APCmgalo48@gmail.com>
2015-03-29 20:32:24 +0800

    fix

--- trunk/Application/marktroibags3.0/frontend/views/Product/view.php
+++ trunk/Application/marktroibags3.0/frontend/views/Product/view.php
@@ -33,7 +33,7 @@ $this->params['breadcrumbs'][] = $this->title;
         
     ]) ?>
     <?= $ImageName = $model->name; ?>
-    <?= Html::a('View Product','uploads/'.$ImageName.'.png') ?>
+    <?= Html::a('View Product','uploads/'.$ImageName.'.jpg') ?>
 
     
 </div>
