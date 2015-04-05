092d93e5bb2ce8b0da25971952e95e8436709f55
Eduardo B. Nipas Jr <EdNipas@gmail.com>
2015-04-05 18:07:39 +0800

    modified about page

--- trunk/Application/marktroibags3.0/frontend/views/site/about.php
+++ trunk/Application/marktroibags3.0/frontend/views/site/about.php
@@ -8,7 +8,13 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>This is the About page. You may modify the following file to customize its content:</p>
+    <p>TMark Troi Bags is a duly recognized business entity by the Department of Trade and Industry and Department of Labor and Employment established in 2001 under the name MINT Bags and Promotional Enterprises, through the leadership and management of Ms. Luzviminda Mendoza 
 
-    <code><?= __FILE__ ?></code>
+		Her expertise has gained the trust and respect of clients such as: Unilever, Mars Food Inc.-Singapore, Veterans Bank, BPI, Chinabank, Maybank, Phil. Red Cross, Asian Life Insurance, Sara Lee, Rustans Phil. and more 
+
+			By 2008, The company changed the company name to Mark Troi Bags</p>
+
+			
+
+ <a href="https://www.facebook.com/pages/Mark-Troi-Bags/1559827930943371"  target="_blank">Visit us on Facebook</a>
 </div>
